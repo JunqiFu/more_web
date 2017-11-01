6da6f5e96e5158b89fb42b97ccc26d25174fa182
liangkanglin <liangkanglin@16195a95-72d7-4d43-8a5a-5b6d0db5564d>
2017-11-01 08:14:27 +0000

    创建DB链接驱动

---
+++ Shoping/src/system/db/DBcon.java
@@ -0,0 +1,35 @@
+package system.db;
+
+
+import java.sql.*;
+
+public class DBcon {
+	
+	private Connection con;
+	
+
+	public Connection getConnection(){		
+		try{
+			//Class.forName("com.mysql.jdbc.Driver");
+			//con=DriverManager.getConnection("jdbc:mysql://localhost/mysql","root","root");
+		
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/shopping?"
+					+ "useUnicode=true&characterEncoding=UTF8";
+			con = DriverManager.getConnection(url, "root", "root");
+		}catch(Exception e){
+			e.printStackTrace();
+		}	
+		return con;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		DBcon db=new DBcon();
+		System.out.println(db.getConnection());
+
+	}
+
+}
