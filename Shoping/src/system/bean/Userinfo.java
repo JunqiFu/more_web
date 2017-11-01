39e790d417c44ca99ab83f766ac5d4d08d041f15
liangkanglin <liangkanglin@16195a95-72d7-4d43-8a5a-5b6d0db5564d>
2017-11-01 08:24:37 +0000

    创建用户bean

---
+++ Shoping/src/system/bean/Userinfo.java
@@ -0,0 +1,68 @@
+package system.bean;
+
+public class Userinfo {
+	private int id;
+	private String username;
+	private String password;
+	private String phone;
+	private String email;
+	private String address;
+	private String address_o;
+	private String address_t;
+	private int power;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAddress_o() {
+		return address_o;
+	}
+	public void setAddress_o(String address_o) {
+		this.address_o = address_o;
+	}
+	public String getAddress_t() {
+		return address_t;
+	}
+	public void setAddress_t(String address_t) {
+		this.address_t = address_t;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+}
