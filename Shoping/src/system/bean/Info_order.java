e743fde378617f95f6fb96dab7354305e9b7be69
liangkanglin <liangkanglin@16195a95-72d7-4d43-8a5a-5b6d0db5564d>
2017-11-22 08:49:17 +0000

    创建了订单分表类(bean)

---
+++ Shoping/src/system/bean/Info_order.java
@@ -0,0 +1,26 @@
+package system.bean;
+
+public class Info_order {
+	private int o_id;//订单的id
+	private int g_id;//商品ID
+	private int i_price;//当时买的时候的价格
+	public int getO_id() {
+		return o_id;
+	}
+	public void setO_id(int o_id) {
+		this.o_id = o_id;
+	}
+	public int getG_id() {
+		return g_id;
+	}
+	public void setG_id(int g_id) {
+		this.g_id = g_id;
+	}
+	public int getI_price() {
+		return i_price;
+	}
+	public void setI_price(int i_price) {
+		this.i_price = i_price;
+	}
+	
+}
