e95458639e8887005c02d3c8d98e8baf936fd3e7
liangkanglin <liangkanglin@16195a95-72d7-4d43-8a5a-5b6d0db5564d>
2017-11-22 08:32:14 +0000

    创建了商品类(bean)

---
+++ Shoping/src/system/bean/Goods.java
@@ -0,0 +1,118 @@
+package system.bean;
+
+public class Goods {
+	private int g_id;//商品的id
+	private String g_name;//商品的名字
+	private String g_detail;//详情信息
+	private float g_cost;//原价
+	private float g_price;//现价
+	private String g_color;//颜色
+	private String g_color_o;//颜色一
+	private String g_color_t;//颜色二
+	private String g_address;//生产地址
+	private String g_picture;//图片一
+	private String g_pictrue2;//图片2
+	private String g_picture3;//图片3
+	private String g_picture4;//图片4
+	private String g_picture5;//图片5
+	private String g_info;//信息
+	private String g_size;//规格
+
+	public int getG_id() {
+		return g_id;
+	}
+	public void setG_id(int g_id) {
+		this.g_id = g_id;
+	}
+	public String getG_name() {
+		return g_name;
+	}
+	public void setG_name(String g_name) {
+		this.g_name = g_name;
+	}
+	public String getG_detail() {
+		return g_detail;
+	}
+	public void setG_detail(String g_detail) {
+		this.g_detail = g_detail;
+	}
+	public float getG_cost() {
+		return g_cost;
+	}
+	public void setG_cost(float g_cost) {
+		this.g_cost = g_cost;
+	}
+	public float getG_price() {
+		return g_price;
+	}
+	public void setG_price(float g_price) {
+		this.g_price = g_price;
+	}
+	public String getG_color() {
+		return g_color;
+	}
+	public void setG_color(String g_color) {
+		this.g_color = g_color;
+	}
+	public String getG_color_o() {
+		return g_color_o;
+	}
+	public void setG_color_o(String g_color_o) {
+		this.g_color_o = g_color_o;
+	}
+	public String getG_color_t() {
+		return g_color_t;
+	}
+	public void setG_color_t(String g_color_t) {
+		this.g_color_t = g_color_t;
+	}
+	public String getG_address() {
+		return g_address;
+	}
+	public void setG_address(String g_address) {
+		this.g_address = g_address;
+	}
+	public String getG_picture() {
+		return g_picture;
+	}
+	public void setG_picture(String g_picture) {
+		this.g_picture = g_picture;
+	}
+	public String getG_pictrue2() {
+		return g_pictrue2;
+	}
+	public void setG_pictrue2(String g_pictrue2) {
+		this.g_pictrue2 = g_pictrue2;
+	}
+	public String getG_picture3() {
+		return g_picture3;
+	}
+	public void setG_picture3(String g_picture3) {
+		this.g_picture3 = g_picture3;
+	}
+	public String getG_picture4() {
+		return g_picture4;
+	}
+	public void setG_picture4(String g_picture4) {
+		this.g_picture4 = g_picture4;
+	}
+	public String getG_picture5() {
+		return g_picture5;
+	}
+	public void setG_picture5(String g_picture5) {
+		this.g_picture5 = g_picture5;
+	}
+	public String getG_info() {
+		return g_info;
+	}
+	public void setG_info(String g_info) {
+		this.g_info = g_info;
+	}
+	public String getG_size() {
+		return g_size;
+	}
+	public void setG_size(String g_size) {
+		this.g_size = g_size;
+	}
+
+}
