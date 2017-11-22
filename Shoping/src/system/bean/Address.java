bba29b7d2f8aa030c22c3486442073b76d641c54
liangkanglin <liangkanglin@16195a95-72d7-4d43-8a5a-5b6d0db5564d>
2017-11-22 08:45:03 +0000

    创建了地址类(bean)

---
+++ Shoping/src/system/bean/Address.java
@@ -0,0 +1,19 @@
+package system.bean;
+
+public class Address {
+	private String u_username;//用户名
+	private String address;//地址
+	public String getU_username() {
+		return u_username;
+	}
+	public void setU_username(String u_username) {
+		this.u_username = u_username;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
