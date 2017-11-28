78aa53703cebb3632e80f7d773276cabfd8be634
liangkanglin <liangkanglin@16195a95-72d7-4d43-8a5a-5b6d0db5564d>
2017-11-28 09:37:26 +0000

    index中的热门推荐完成

--- Shoping/src/system/dao/impl/GoodsDAOImpl.java
+++ Shoping/src/system/dao/impl/GoodsDAOImpl.java
@@ -17,6 +17,7 @@ public class GoodsDAOImpl {
 	private ResultSet rs;
 	private DBcon db;
 	private String g_class="大牌专享";
+	private String g_fire="热门推荐";
 	
 	//链接数据库
 	public GoodsDAOImpl(){
@@ -33,6 +34,46 @@ public class GoodsDAOImpl {
 			String sql="SELECT * FROM goods WHERE g_class=?";
 			pstmt=con.prepareStatement(sql);
 			pstmt.setString(1, g_class);
+			rs=pstmt.executeQuery();
+            while(rs.next()){				
+				Goods good = new Goods();
+				good.setG_id(rs.getInt("g_id"));
+				good.setG_name(rs.getString("g_name"));
+				good.setG_detail(rs.getString("g_detail"));
+				good.setG_cost(rs.getInt("g_cost"));
+				good.setG_price(rs.getInt("g_price"));
+				good.setG_brand(rs.getString("g_brand"));
+				good.setG_color(rs.getString("g_color"));
+				good.setG_color_o(rs.getString("g_color_o"));
+				good.setG_color_t(rs.getString("g_color_t"));
+				good.setG_address(rs.getString("g_address"));
+				good.setG_picture(rs.getString("g_picture"));
+				good.setG_picture2(rs.getString("g_picture2"));
+				good.setG_picture3(rs.getString("g_picture3"));
+				good.setG_picture4(rs.getString("g_picture4"));
+				good.setG_picture5(rs.getString("g_picture5"));
+				good.setG_info(rs.getString("g_info"));
+				good.setG_size(rs.getString("g_size"));
+				good.setG_class(rs.getString("g_class"));
+				goods.add(good);
+			}						
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return goods;
+	}
+	/**
+	 * 下面这是来获取热门专享的商品
+	 * 
+	 * */
+	public List<Goods> getFireGoods(){
+		List<Goods> goods = new ArrayList<Goods>();	
+		try {
+			con=db.getConnection();
+			String sql="SELECT * FROM goods WHERE g_class=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, g_fire);
 			rs=pstmt.executeQuery();
             while(rs.next()){				
 				Goods good = new Goods();
