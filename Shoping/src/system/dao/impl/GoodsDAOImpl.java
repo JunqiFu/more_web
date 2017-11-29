ce07b8d53ebb60e545e78946176085c550e526a8
liangkanglin <liangkanglin@16195a95-72d7-4d43-8a5a-5b6d0db5564d>
2017-11-29 06:59:48 +0000

    搜索商品页面完成

--- Shoping/src/system/dao/impl/GoodsDAOImpl.java
+++ Shoping/src/system/dao/impl/GoodsDAOImpl.java
@@ -95,7 +95,6 @@ public class GoodsDAOImpl {
 				good.setG_info(rs.getString("g_info"));
 				good.setG_size(rs.getString("g_size"));
 				good.setG_class(rs.getString("g_class"));
-				System.err.println(good.getG_brand());
 				goods.add(good);
 			}						
 		} catch (SQLException e) {
@@ -103,6 +102,46 @@ public class GoodsDAOImpl {
 			e.printStackTrace();
 		}		
 		return goods;
+	}
+	public List<Goods> searchGoods(String key) {
+		List<Goods> goods = new ArrayList<Goods>();	
+		try {
+			con=db.getConnection();
+			String sql="SELECT * FROM goods WHERE g_name like ? or g_detail like ? or g_brand like ?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, "%" + key + "%");
+			pstmt.setString(2, "%" + key + "%");
+			pstmt.setString(3, "%" + key + "%");
+			rs=pstmt.executeQuery();
+			while (rs.next()) {
+				Goods good = new Goods();
+				good.setG_id(rs.getInt("g_id"));
+				good.setG_name(rs.getString("g_name"));
+				good.setG_detail(rs.getString("g_detail"));
+				good.setG_cost(rs.getInt("g_cost"));
+				good.setG_price(rs.getInt("g_price"));
+				good.setG_brand(rs.getString("g_brand"));
+				good.setG_color(rs.getString("g_color"));
+				good.setG_color_o(rs.getString("g_color_o"));
+				good.setG_color_t(rs.getString("g_color_t"));
+				good.setG_address(rs.getString("g_address"));
+				good.setG_picture(rs.getString("g_picture"));
+				good.setG_picture2(rs.getString("g_picture2"));
+				good.setG_picture3(rs.getString("g_picture3"));
+				good.setG_picture4(rs.getString("g_picture4"));
+				good.setG_picture5(rs.getString("g_picture5"));
+				good.setG_info(rs.getString("g_info"));
+				good.setG_size(rs.getString("g_size"));
+				good.setG_class(rs.getString("g_class"));
+				System.err.println(good.getG_brand());
+				goods.add(good);
+			}
+			return goods;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 		
 
